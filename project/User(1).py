5d8aaf2ca27af3be43fd878b20e89775434643b5
Makise-KurisuR <2687726146@qq.com>
2024-12-10 18:30:45 +0000

    Add files via upload

---
+++ project/User(1).py
@@ -0,0 +1,16 @@
+from Database import Database
+
+
+class User:
+
+    # minimal user class to facilitate shopping basket function
+    # would extend usual user object from previous SSH app in practise
+    # author:
+
+    def __init__(self, user_id):
+        self.user_id = user_id
+
+    # returns name associated with user_id in database
+    def get_name(self):
+        return Database().get_name(self.user_id)
+
---
+++ project/helpers(1).py
@@ -0,0 +1,10 @@
+import json
+
+# returns saved order split agreed upon by the house
+def get_order_split():
+    with open("logs/order_constants.json") as json_file:
+        json_data = json.load(json_file)
+    for constant in json_data:
+        if constant["type"] == "order_split":
+            return constant["value"]
+
